6b0dd126d727e8bf97cbc7e3e97ddcbe55282076
xiangshanhu <956682073@qq.com>
2020-10-04 16:52:22 +0800

    hhh

---
+++ build/precache-manifest.76232979ee91d4403b3b5c5b20e76fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d8e65e963777e05e1fd291f9522cf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85e6e348f250be2e8c44",
+    "url": "/static/js/2.2984d714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2984d714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1729f189ac2ea923c198",
+    "url": "/static/js/main.cb7f16a8.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
