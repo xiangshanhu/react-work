9c816aa6fad79b4c9de6f95dc876673ae75378d0
xiangshanhu <956682073@qq.com>
2020-10-18 16:39:48 +0800

    work-2
    Signed-off-by: xiangshanhu <956682073@qq.com>

---
+++ work-2/build/precache-manifest.1ed69070bc18cefec9aa8c6136b74e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3747550a0faaee09342f5042420b7b80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3687febb911f9b7f08",
+    "url": "/static/css/main.1963f320.chunk.css"
+  },
+  {
+    "revision": "7d69703027243821996b",
+    "url": "/static/js/2.95543224.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.95543224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3687febb911f9b7f08",
+    "url": "/static/js/main.d777b6cf.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
