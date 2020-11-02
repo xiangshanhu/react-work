a4e5b653ec424c16463aac9dde50aba41f2fa184
xiangdahai <mazipeng@yhsec.net>
2020-11-02 14:17:01 +0800

    hbctf
    Signed-off-by: xiangdahai <mazipeng@yhsec.net>

---
+++ hbctf/build/precache-manifest.da291032f2afe1ecb52dafe3e61fd7e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59225ba459b573cb7c3d46a52e3f0b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82bdd8cd9f949c2891cf",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "ec9b2bff22878b5b94a0",
+    "url": "/static/css/main.80709d85.chunk.css"
+  },
+  {
+    "revision": "82bdd8cd9f949c2891cf",
+    "url": "/static/js/2.f814dbb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f814dbb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9b2bff22878b5b94a0",
+    "url": "/static/js/main.4cd73660.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "6e491369e31d92a71ea176bbe9d36e84",
+    "url": "/static/media/bg1 1.6e491369.png"
+  }
+]);
