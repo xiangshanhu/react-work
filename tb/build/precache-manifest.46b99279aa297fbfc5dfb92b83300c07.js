e223b3b02c1fc475a0c97d34352a979d29220876
xiangdahai <mazipeng@yhsec.net>
2020-11-09 11:36:59 +0800

    tb
    Signed-off-by: xiangdahai <mazipeng@yhsec.net>

---
+++ tb/build/precache-manifest.46b99279aa297fbfc5dfb92b83300c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d9b8838e032dc31f7f8eae3fe06090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76c076354e8a690087f2",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "76c076354e8a690087f2",
+    "url": "/static/js/2.982776c6.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.982776c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3d18c8f3ee57d2901d",
+    "url": "/static/js/main.af4e6b28.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
